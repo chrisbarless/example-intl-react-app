80b1ef9f53054cff81ac3871a051a31bb7b3288d
Chris Barless <chris@barless.co>
2020-04-17 16:52:57 -0400

    Index file and typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier/react',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
 };
---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+const App = <div>Form</div>;
+
+export default App;
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import {IntlProvider, FormattedMessage} from 'react-intl';
+
+import messages from './messages';
+
+const App: React.FC = () => (
+  <div>
+    <h1>
+      <FormattedMessage id="formHeader" />
+    </h1>
+  </div>
+);
+
+ReactDOM.render(
+  <IntlProvider locale="en" messages={messages.en}>
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+);
