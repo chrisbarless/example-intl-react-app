f24a52365d6844f3da3d090cd96a4b3f1219ba6a
Chris Barless <chris@barless.co>
2020-04-20 12:31:19 -0400

    Add language switcher

--- src/app.tsx
+++ src/app.tsx
@@ -2,19 +2,20 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import {IntlProvider, FormattedMessage} from 'react-intl';
 
+import LanguageSwitcher from './components/language-switcher';
 import messages from './messages.yaml';
 
-const App: React.FC = () => (
-  <div>
-    <h1>
-      <FormattedMessage id="formHeader" />
-    </h1>
-  </div>
-);
+const App: React.FC = () => {
+  const [locale, setLocale] = React.useState('en-US');
 
-ReactDOM.render(
-  <IntlProvider locale="en" messages={messages.en}>
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
-);
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <LanguageSwitcher onChange={setLocale} locale={locale} />
+      <h1>
+        <FormattedMessage id="formHeader" />
+      </h1>
+    </IntlProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/components/language-switcher/language-switcher.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import './language-switcher.scss';
+
+const languages = [
+  {label: 'EN', value: 'en-US'},
+  {label: 'ES', value: 'es-US'},
+];
+
+type LanguageSwitcherProps = {
+  locale: 'en-US' | 'es-US';
+};
+
+const LanguageSwitcher: React.FC = ({
+  locale,
+  onChange,
+}: LanguageSwitcherProps) => (
+  <select
+    className="language-switcher"
+    name="locale"
+    defaultValue={locale}
+    id="locale"
+    onChange={(event): void => onChange(event.target.value)}
+  >
+    {languages.map(({label, value}) => (
+      <option key={value} value={value}>
+        {label}
+      </option>
+    ))}
+  </select>
+);
+
+export default LanguageSwitcher;
